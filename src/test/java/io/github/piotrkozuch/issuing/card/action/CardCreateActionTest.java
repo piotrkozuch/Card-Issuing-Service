4a546725921ab2a813330fec051feb723322f210
piotr.kozuch <piotr.kozuch19@gmail.com>
2023-01-15 21:09:37 +0100

    Added Card and CardSensitiveDetails models

--- src/test/java/io/github/piotrkozuch/issuing/card/action/CardCreateActionTest.java
+++ src/test/java/io/github/piotrkozuch/issuing/card/action/CardCreateActionTest.java
@@ -1,5 +1,18 @@
 package io.github.piotrkozuch.issuing.card.action;
 
+import io.github.piotrkozuch.issuing.card.repository.CardRepository;
+import io.github.piotrkozuch.issuing.card.service.CardSensitiveDetailsService;
+import io.github.piotrkozuch.issuing.cardholder.repository.CardholderRepository;
+
+import static org.mockito.Mockito.mock;
+
 class CardCreateActionTest {
+
+    private final CardRepository cardRepository = mock(CardRepository.class);
+    private final CardholderRepository cardholderRepository = mock(CardholderRepository.class);
+    private final CardSensitiveDetailsService cardSensitiveDetailsService = mock(CardSensitiveDetailsService.class);
+
+    private final CardCreateAction action = new CardCreateAction(cardRepository, cardholderRepository, cardSensitiveDetailsService);
+
     //TODO: implement tests
 }
